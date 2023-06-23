45f77a85c2273131d136a69520c72ed4119ace56
RajeshOmbase <rajesh.ombase@gmail.com>
2023-06-23 23:55:44 +0530

    update 7

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -2,7 +2,67 @@ void call(){
 node {
     stage("Maven: Build"){
         println "build from the maven library"	
-        sleep 60		
+        steps {
+
+                // Checkout code from SVN repository changes
+
+                checkout([$class: 'SubversionSCM',
+
+                additionalCredentials: [],
+
+                excludedCommitMessages: '',
+
+                excludedRegions: '',
+
+                excludedRevprop: '',
+
+                excludedUsers: '',
+
+                filterChangelog: false,
+
+                ignoreDirPropChanges: false,
+
+                includedRegions: '',
+
+                locations: [[credentialsId: 'svn_credential_pipeline',
+
+                           depthOption: 'infinity',
+
+                           ignoreExternalsOption: true,
+
+                           remote: 'https://svn.riouxsvn.com/dmifactory']],
+
+                workspaceUpdater: [$class: 'UpdateUpdater']])
+
+                script {
+
+                    def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+
+                // Print current directory
+
+                    echo "Current directory: ${currentDir}"
+
+                // List contents of the current directory
+
+                    sh "ls ${currentDir}"
+
+                //var = echo 'Current directory: ' + pwd()
+
+                    sh 'ls /home/jenkins/agent/workspace/vikas_test_pipeline_new/dmifactory'
+
+                   
+
+                }
+
+                // Set the path to the build.xml file
+
+               
+
+            }
+
+
+
+
+        }		
     }
-}
 }
