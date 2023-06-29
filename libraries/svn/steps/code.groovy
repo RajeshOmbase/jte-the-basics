7c0905c779b99cb66bf09e14ae13c992d65dfd2a
Vikas Salaria <vsalaria@dminc.com>
2023-06-29 18:18:45 +0530

    commit changes related to svn8

--- libraries/svn/steps/code.groovy
+++ libraries/svn/steps/code.groovy
@@ -1,27 +1,26 @@
 // Fetch code from svn repository
 
-
-void call()
+node 
 {
-    node 
-{
-    stage('svn:code') {
-        String svn_repo = config.svn_repo
-        checkout([$class: 'SubversionSCM', 
-        additionalCredentials: [], 
-        excludedCommitMessages: '', 
-        excludedRegions: '', 
-        excludedRevprop: '', 
-        excludedUsers: '', 
-        filterChangelog: false, 
-        ignoreDirPropChanges: false, 
-        includedRegions: '', 
-        locations: [[credentialsId: 'svn_credential_pipeline', 
-                    depthOption: 'infinity', 
-                    ignoreExternalsOption: true, 
-                    remote: svn_repo]],
-        workspaceUpdater: [$class: 'UpdateUpdater']])
-        sh 'ls -l'
+    void call()
+    {
+        stage('svn:code') {
+            String svn_repo = config.svn_repo
+            checkout([$class: 'SubversionSCM',
+                additionalCredentials: [], 
+                excludedCommitMessages: '', 
+                excludedRegions: '', 
+                excludedRevprop: '', 
+                excludedUsers: '', 
+                filterChangelog: false, 
+                ignoreDirPropChanges: false, 
+                includedRegions: '', 
+                locations: [[credentialsId: 'svn_credential_pipeline', 
+                            depthOption: 'infinity', 
+                            ignoreExternalsOption: true, 
+                            remote: svn_repo]],
+                workspaceUpdater: [$class: 'UpdateUpdater']])
+            sh 'ls -l'
         // Set the path to the build.xml file
         
     
