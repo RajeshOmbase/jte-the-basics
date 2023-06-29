7793fa70b265e1fe85613fd5bf36361beb9b272a
Vikas Salaria <vsalaria@dminc.com>
2023-06-29 17:27:16 +0530

    commit changes related to svn2

--- libraries/ant/steps/build.groovy
+++ libraries/ant/steps/build.groovy
@@ -2,25 +2,17 @@ void call(){
     def tools = [ant: 'Ant_Home']
     stage("Ant: Build"){
         println "build from the Ant library"
-        script {
-
-            def currentDir = sh(returnStdout: true,script: 'pwd').trim()
-
-        // Print current directory
-
-            echo "Current directory: ${currentDir}"
-
-        // List contents of the current directory
-
-            sh "ls ${currentDir}"
-            def buildXmlPath = "${env.WORKSPACE}/dmifactory/build.xml"
-            def buildXmlContent = readFile("${env.WORKSPACE}/dmifactory/build.xml")
-            echo "Build.xml Content:\n${buildXmlContent}"
-            //def antHome = tool 'Ant_Home'
-            withAnt(installation: 'Ant_Home') {
-                dir("${currentDir}/dmifactory") {
-                    sh 'ant clean compile jspDeploy target war'
-                } 
+        def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+        echo "Current directory: ${currentDir}"
+        sh "ls ${currentDir}"
+        def buildXmlPath = "${env.WORKSPACE}/dmifactory/build.xml"
+        def buildXmlContent = readFile("${env.WORKSPACE}/dmifactory/build.xml")
+        echo "Build.xml Content:\n${buildXmlContent}"
+        //def antHome = tool 'Ant_Home'
+        withAnt(installation: 'Ant_Home') {
+            dir("${currentDir}/dmifactory") {
+                sh 'ant clean compile jspDeploy target war'
+            } 
             
     }
 
