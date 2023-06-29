e6e4151926f556904d39ccc4539b31a032705479
Vikas Salaria <vsalaria@dminc.com>
2023-06-29 18:46:58 +0530

    commit changes related to svn11

--- libraries/sonarqube/steps/secure.groovy
+++ libraries/sonarqube/steps/secure.groovy
@@ -1,23 +1,22 @@
+stage("SonarQube: Static Code Analysis")
 {
-    stage("SonarQube: Static Code Analysis"){
-        def currentDir = sh(returnStdout: true,script: 'pwd').trim()
-        String projectKey = config.projectKey
-        String sonarHostUrl = config.sonarHostUrl
+    def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+    String projectKey = config.projectKey
+    String sonarHostUrl = config.sonarHostUrl
 
-        println "static code analysis from the sonarqube library"
-         script {
-                    echo "SonarQube analysis"
-                    def scannerHome = tool 'SonarQubeScanner'
-                    withSonarQubeEnv('sonarserver') {
-                        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.host.url=${sonarHostUrl} -Dsonar.sources=src -Dsonar.java.binaries=${currentDir}/dmifactory/build -Dsonar.log.level=DEBUG"
+    println "static code analysis from the sonarqube library"
+        script {
+            echo "SonarQube analysis"
+            def scannerHome = tool 'SonarQubeScanner'
+            withSonarQubeEnv('sonarserver') {
+                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.host.url=${sonarHostUrl} -Dsonar.sources=src -Dsonar.java.binaries=${currentDir}/dmifactory/build -Dsonar.log.level=DEBUG"
 
-                        // dmifactory/build
-                        //-Dsonar.log
-                        
-                    }           
+                    // dmifactory/build
+                    //-Dsonar.log
+                    
+                }           
 
-            }
+        }
 
     }
 
-}
