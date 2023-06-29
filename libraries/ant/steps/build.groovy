c40692b5587c8c40ff0fad71e94d00b83934b6ee
Vikas Salaria <vsalaria@dminc.com>
2023-06-29 22:19:10 +0530

    commit changes related to svn23

--- libraries/ant/steps/build.groovy
+++ libraries/ant/steps/build.groovy
@@ -72,7 +72,7 @@ void call(){
             println "static code analysis from the sonarqube library and test1" 
             script {
                 echo "SonarQube analysis"
-                def scannerHome = [scannerHome: 'SonarQubeScanner']
+                def scannerHome = tool 'SonarQubeScanner'
                 withSonarQubeEnv('SonarQubeScanner') {
                     sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.host.url=${sonarHostUrl} -Dsonar.sources=src -Dsonar.java.binaries=${currentDir}/dmifactory/build -Dsonar.log.level=DEBUG"
 
