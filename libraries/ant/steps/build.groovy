5d91a769542bb5c67216639fe0db912a4614ca2f
Vikas Salaria <vsalaria@dminc.com>
2023-06-29 18:12:17 +0530

    commit changes related to svn7

--- libraries/ant/steps/build.groovy
+++ libraries/ant/steps/build.groovy
@@ -1,28 +1,30 @@
 void call()
-{
-    def tools = [ant: 'Ant_Home']
-    stage("Ant: Build"){
-        script
-        {
-            println "build from the Ant library"
-            // def currentDir = sh(returnStdout: true,script: 'pwd').trim()
-            // echo "Current directory: ${currentDir}"
-            // sh "ls ${currentDir}"
-            def buildXmlPath = "${env.WORKSPACE}/dmifactory/build.xml"
-            def buildXmlContent = readFile("${env.WORKSPACE}/dmifactory/build.xml")
-            echo "Build.xml Content:\n${buildXmlContent}"
-            //def antHome = tool 'Ant_Home'
-            withAnt(installation: 'Ant_Home') {
-                dir("${currentDir}/dmifactory") {
-                    sh 'ant clean compile jspDeploy target war'
-                } 
+{   
+    def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+    echo "Current directory: ${currentDir}"
+    // def tools = [ant: 'Ant_Home']
+    // stage("Ant: Build"){
+    //     script
+    //     {
+    //         println "build from the Ant library"
+    //         // def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+    //         // echo "Current directory: ${currentDir}"
+    //         // sh "ls ${currentDir}"
+    //         def buildXmlPath = "${env.WORKSPACE}/dmifactory/build.xml"
+    //         def buildXmlContent = readFile("${env.WORKSPACE}/dmifactory/build.xml")
+    //         echo "Build.xml Content:\n${buildXmlContent}"
+    //         //def antHome = tool 'Ant_Home'
+    //         withAnt(installation: 'Ant_Home') {
+    //             dir("${currentDir}/dmifactory") {
+    //                 sh 'ant clean compile jspDeploy target war'
+    //             } 
                 
-        }
-        }
+    //     }
+    //     }
 
        
 
-        }
+    //     }
 
 
 }
