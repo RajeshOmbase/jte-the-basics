28a5bfd40991bf504dfa9077bdf83079a283542e
RajeshOmbase <rajesh.ombase@gmail.com>
2023-07-01 00:14:06 +0530

    a

--- libraries/rsvn/steps/code.groovy
+++ libraries/rsvn/steps/code.groovy
@@ -1,7 +1,10 @@
+package libraries.sdp
+
+import hudson.AbortException
 
 // Fetch code from svn repository
 @Validate
-void call()
+void call(context)
 {
         node{
         stage('svn:code') {
@@ -33,10 +36,11 @@ void call()
 			//dir("${currentDir}/dmifactory")
 			dir('dmifactory'){
 			sh 'ls -l'
+			stash name: 'workspace', allowEmpty: true, useDefaultExcludes: false
 			//create_workspace_stash(name: 'my-artifacts', includes: '/home/jenkins/agent/workspace/Rajesh-JTE-Pipeline/dmifactory/**')
-			create_workspace_stash('my-artifacts') {
-            dir('/home/jenkins/agent/workspace/Rajesh-JTE-Pipeline/dmifactory') {
-            stash(includes: '**', name: 'my-artifacts')
+			//create_workspace_stash('my-artifacts') {
+            //dir('/home/jenkins/agent/workspace/Rajesh-JTE-Pipeline/dmifactory') {
+            //stash(includes: '**', name: 'my-artifacts')
     }
 }
 			}
