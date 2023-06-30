9be43c0a6e9babcbc9acc19b3aab04b7df76ffb3
RajeshOmbase <rajesh.ombase@gmail.com>
2023-06-30 16:26:27 +0530

    uncheck

--- libraries/ant/steps/buildcombined.groovy
+++ libraries/ant/steps/buildcombined.groovy
@@ -5,7 +5,7 @@ void call(){
     stage("Ant: Build"){
         String svn_repo = config.svn_repo
         println "build from the Ant library"
-		echo ${env.JOB_NAME}	
+		//echo ${env.JOB_NAME}	
         
 
 // Checkout code from SVN repository changes
