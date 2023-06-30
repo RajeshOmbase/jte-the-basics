17c1557a398a0edb1ad7d853712dcec872f22b27
RajeshOmbase <rajesh.ombase@gmail.com>
2023-06-30 23:17:34 +0530

    New Test Repo Rajesh

---
+++ libraries/rant/steps/build_old.groovy
@@ -0,0 +1,31 @@
+void call()
+{   
+    def another_script = load 'jte-the-basics/libraries/ant/steps/build.groovy'
+    def tools = [ant: 'Ant_Home']
+    stage("Ant: Build"){
+        script
+        {
+            println "build from the Ant library"
+            // def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+            // echo "Current directory: ${currentDir}"
+            // sh "ls ${currentDir}"
+            def buildXmlPath = "${env.WORKSPACE}/dmifactory/build.xml"
+            def buildXmlContent = readFile("${env.WORKSPACE}/dmifactory/build.xml")
+            echo "Build.xml Content:\n${buildXmlContent}"
+            //def antHome = tool 'Ant_Home'
+            withAnt(installation: 'Ant_Home') {
+                dir("${currentDir}/dmifactory") {
+                    sh 'ant clean compile jspDeploy target war'
+                } 
+                
+        }
+        }
+
+       
+
+       }
+
+
+}
+
+    
---
+++ libraries/rsvn/steps/code.groovy
@@ -0,0 +1,41 @@
+
+// Fetch code from svn repository
+@Validate
+void call()
+{
+        node{
+        stage('svn:code') {
+            String svn_repo = config.svn_repo
+            checkout([$class: 'SubversionSCM',
+                additionalCredentials: [], 
+                excludedCommitMessages: '', 
+                excludedRegions: '', 
+                excludedRevprop: '', 
+                excludedUsers: '', 
+                filterChangelog: false, 
+                ignoreDirPropChanges: false, 
+                includedRegions: '', 
+                locations: [[credentialsId: 'svn_credential_pipeline', 
+                            depthOption: 'infinity', 
+                            ignoreExternalsOption: true, 
+                            remote: svn_repo]],
+                workspaceUpdater: [$class: 'UpdateUpdater']])
+            sh 'ls -l'
+
+           def currentDir = sh(returnStdout: true,script: 'pwd').trim()
+            echo context
+        // Print current directory
+
+            echo "Current directory: ${currentDir}"
+
+        // List contents of the current directory
+
+            sh "ls ${currentDir}"
+        
+    
+
+}
+
+        }
+
+}
