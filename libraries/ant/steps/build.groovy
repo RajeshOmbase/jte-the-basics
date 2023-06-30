07b712f0cbbbe51f976f3186e7b2e4fe46d55a07
Vikas Salaria <vsalaria@dminc.com>
2023-06-30 10:55:05 +0530

    commit changes related to svn34

--- libraries/ant/steps/build.groovy
+++ libraries/ant/steps/build.groovy
@@ -23,9 +23,9 @@ void call()
             //sh "ls ${currentDir}"
 
             //def tools = [ant: 'Ant_Home']
-            def buildXmlPath = "${currentDir}/dmifactory/build.xml"
+            def buildXmlPath = currentDir + "/dmifactory/build.xml"
             echo "Build.xml Path: ${buildXmlPath}"
-            def buildXmlContent = readFile("${currentDir}/dmifactory/build.xml")
+            def buildXmlContent = readFile(buildXmlPath)
             echo "Build.xml Content:\n${buildXmlContent}"
             //def antHome = tool 'Ant_Home'
             withAnt(installation: 'Ant_Home') {
