c147f736f94cdcb10e65a02d9c64297f33154ac3
Vikas Salaria <vsalaria@dminc.com>
2023-06-30 18:09:52 +0530

    commit changes related to svn38

--- libraries/svn/steps/code.groovy
+++ libraries/svn/steps/code.groovy
@@ -1,34 +1,33 @@
 // Fetch code from svn repository
 void call()
 {
-    node {
-        stage('svn:code') {
-            String svn_repo = config.svn_repo
-            checkout([$class: 'SubversionSCM',
-                additionalCredentials: [], 
-                excludedCommitMessages: '', 
-                excludedRegions: '', 
-                excludedRevprop: '', 
-                excludedUsers: '', 
-                filterChangelog: false, 
-                ignoreDirPropChanges: false, 
-                includedRegions: '', 
-                locations: [[credentialsId: 'svn_credential_pipeline', 
-                            depthOption: 'infinity', 
-                            ignoreExternalsOption: true, 
-                            remote: svn_repo]],
-                workspaceUpdater: [$class: 'UpdateUpdater']])
-            sh 'ls -l'
+    stage('svn:code') {
+        String svn_repo = config.svn_repo
+        checkout([$class: 'SubversionSCM',
+        additionalCredentials: [], 
+        excludedCommitMessages: '', 
+        excludedRegions: '', 
+        excludedRevprop: '', 
+        excludedUsers: '', 
+        filterChangelog: false, 
+        ignoreDirPropChanges: false, 
+        includedRegions: '', 
+        locations: [[credentialsId: 'svn_credential_pipeline', 
+                    depthOption: 'infinity', 
+                    ignoreExternalsOption: true, 
+                    remote: svn_repo]],
+        workspaceUpdater: [$class: 'UpdateUpdater']])
+        sh 'ls -l'
 
-            env.current_dir = sh(returnStdout: true, script: 'pwd').trim()
-            println "env starts"
-           // sh  current_dir + '/dmifactory'
-            def localStoragePath = env.current_dir
-            println "inspect code starts"
+        // env.current_dir = sh(returnStdout: true, script: 'pwd').trim()
+        //     println "env starts"
+        //    // sh  current_dir + '/dmifactory'
+        //     def localStoragePath = env.current_dir
+        //     println "inspect code starts"
 
-            // Store the local storage path in an environment variable
-            env.LOCAL_STORAGE_PATH = localStoragePath
-            println "inspect code ends"
+        //     // Store the local storage path in an environment variable
+        //     env.LOCAL_STORAGE_PATH = localStoragePath
+        //     println "inspect code ends"
         // Set the path to the build.xml file
         
     
@@ -36,7 +35,3 @@ void call()
 }
 
     }
-
-        
-
-}
