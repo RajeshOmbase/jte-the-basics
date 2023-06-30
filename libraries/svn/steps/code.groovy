28afabd550344f30dc54fd5040c6e22d1c87fbb5
Vikas Salaria <vsalaria@dminc.com>
2023-06-30 22:05:03 +0530

    commit changes related to svn57

--- libraries/svn/steps/code.groovy
+++ libraries/svn/steps/code.groovy
@@ -35,7 +35,7 @@ void call()
             sh "ls -l ${sourceFolder}"
             println "inspect code intermediate"
             // Stash the folder structure and data
-            stash name: 'myStash', includes: "sourceFolder"
+            stash name: 'myStash', includes: sourceFolder
             print "inspect code ends"
             // // Stash the folder structure and data
             // stash name: 'myStash', includes: sourceFolder
