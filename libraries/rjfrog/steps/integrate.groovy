2a83f930ad099a7e35b7325a9937e6a44ec4d229
Vikas Salaria <vsalaria@dminc.com>
2023-07-03 18:57:00 +0530

    commit changes related to svn68

---
+++ libraries/rjfrog/steps/integrate.groovy
@@ -0,0 +1,37 @@
+package libraries.sdp
+
+import hudson.AbortException
+void call
+{
+    node 
+    {
+        stage('integrate:jfrog') {
+            unstash name: 'workspace'
+            String artifactory_url = config.artifactory_url
+            String artifactory_credentials = config.artifactory_credentials
+            String artifactory_global_id = config.artifactory_global_id
+            String artifactory_repo = config.artifactory_repo
+            rtServer (
+                id: artifactory_global_id,
+                url: artifactory_url,
+                credentialsId: artifactory_credentials
+            )   
+
+
+            rtUpload (
+                    serverId: artifactory_global_id,
+                    spec: '''{
+                        "files": [
+                            {
+                                "pattern": "**/*.war",
+                                "target": "java_sample_app"
+                            }
+                        ]
+                    }'''
+                )
+
+        }
+
+         
+    }
+}
