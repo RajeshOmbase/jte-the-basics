51f166a84e279b5157e27280ea7c28222bbb6bdd
Vikas Salaria <vsalaria@dminc.com>
2023-07-04 00:01:38 +0530

    commit changes related to svn72

--- libraries/rssh/steps/sshconnection.groovy
+++ libraries/rssh/steps/sshconnection.groovy
@@ -5,8 +5,10 @@ void call()
         stage('deploy:tomcat') {
             String tomcat_url = config.tomcat_url
             sshagent(['tomcat_deployment_cred']) {
+                println "deploying to tomcat starts"
                 def currentDir = sh(returnStdout: true,script: 'pwd').trim()
                 sh "ls  ${currentDir}/dmifactory"
+                println "deploying to tomcat mid"
                 sh "scp -o StrictHostKeyChecking=no **/*.war ${tomcat_url}:/opt/tomcat/webapps"
                 sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/tomcat/bin/shutdown.sh"
                 sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/tomcat/bin/startup.sh"
