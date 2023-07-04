9a08b0e39a6d5cff21422d675311b9306c33eaed
Vikas Salaria <vsalaria@dminc.com>
2023-07-04 09:39:42 +0530

    commit changes related to svn74

--- libraries/rtomcat/steps/deploy.groovy
+++ libraries/rtomcat/steps/deploy.groovy
@@ -10,7 +10,7 @@ void call()
             String tomcat_url = config.tomcat_url
             sshagent(['tomcat_deployment_cred']) {
                 def currentDir = sh(returnStdout: true,script: 'pwd').trim()
-                sh "ls  ${currentDir}/dmifactory"
+                sh "ls  ${currentDir}"
                 sh "scp -o StrictHostKeyChecking=no **/*.war ${tomcat_url}:/opt/tomcat/webapps"
                 sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/tomcat/bin/shutdown.sh"
                 sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/tomcat/bin/startup.sh"
