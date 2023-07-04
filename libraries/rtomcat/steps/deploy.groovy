150bb0f1c53f342c6b37ed630e4d981e9623052a
Vikas Salaria <vsalaria@dminc.com>
2023-07-04 13:23:57 +0530

    commit changes related to svn79

--- libraries/rtomcat/steps/deploy.groovy
+++ libraries/rtomcat/steps/deploy.groovy
@@ -8,6 +8,24 @@ void call()
         stage('deploy:tomcat') {
             unstash name: 'workspace'
             String tomcat_url = config.tomcat_url
+            // get jenkins slaves ip address through jenkins api
+            def computerApiUrl = "${env.JENKINS_URL}/computer/api/json"
+            def computerApiJson = sh(script: "curl -s ${computerApiUrl}", returnStdout: true).trim()
+            def computerApiData = new JsonSlurper().parseText(computerApiJson)
+            def currentSlave = computerApiData.computer.find { it.displayName == env.NODE_NAME }
+            def slaveIp = currentSlave.actions.find { it._class == 'hudson.slaves.EnvironmentVariablesNodeProperty' }
+                        .envVars.find { it.key == 'JENKINS_SLAVE_IP' }?.value
+
+            if (slaveIp) {
+                println "Jenkins Slave IP: ${slaveIp}"
+            // Use this IP address in your security group configuration or other steps as needed
+            } 
+            else 
+            {
+            error("Failed to retrieve Jenkins Slave IP")
+            }
+            
+    
             sshagent(['tomcat_deployment_cred']) {
                 def currentDir = sh(returnStdout: true,script: 'pwd').trim()
                 sh "ls  ${currentDir}"
@@ -18,6 +36,9 @@ void call()
                 println "deploying to tomcat mid1"
                 sh 'ssh -o StrictHostKeyChecking=no bitnami@ec2-35-83-251-194.us-west-2.compute.amazonaws.com "/opt/bitnami/tomcat/bin/startup.sh"'
                 println "deploying to tomcat mid2"
+
+
+
                 // sh "scp -o StrictHostKeyChecking=no **/*.war ${tomcat_url}:/tmp"
                 // println "deploying to tomcat mid"
                 // sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/bitnami/tomcat/bin/shutdown.sh"
