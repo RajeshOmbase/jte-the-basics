9955f76f3a6c3ddbe7c64bbf410d39174cc13299
Vikas Salaria <vsalaria@dminc.com>
2023-07-04 12:17:09 +0530

    commit changes related to svn78

--- libraries/rtomcat/steps/deploy.groovy
+++ libraries/rtomcat/steps/deploy.groovy
@@ -12,11 +12,11 @@ void call()
                 def currentDir = sh(returnStdout: true,script: 'pwd').trim()
                 sh "ls  ${currentDir}"
                 println "deploying to tomcat starts"
-                sh "scp -o StrictHostKeyChecking=no **/*.war bitnami@35-83-251-194:/opt/tomcat/webapps"
+                sh "scp -o StrictHostKeyChecking=no **/*.war bitnami@ec2-35-83-251-194.us-west-2.compute.amazonaws.com:/opt/bitnami/tomcat/webapps"
                 println "deploying to tomcat mid"
-                sh 'ssh -o StrictHostKeyChecking=no bitnami@35-83-251-194"/opt/tomcat/bin/shutdown.sh"'
+                sh 'ssh -o StrictHostKeyChecking=no bitnami@ec2-35-83-251-194.us-west-2.compute.amazonaws.com "/opt/bitnami/tomcat/bin/shutdown.sh"'
                 println "deploying to tomcat mid1"
-                sh 'ssh -o StrictHostKeyChecking=no bitnami@35-83-251-194"/opt/tomcat/bin/startup.sh"'
+                sh 'ssh -o StrictHostKeyChecking=no bitnami@ec2-35-83-251-194.us-west-2.compute.amazonaws.com "/opt/bitnami/tomcat/bin/startup.sh"'
                 println "deploying to tomcat mid2"
                 // sh "scp -o StrictHostKeyChecking=no **/*.war ${tomcat_url}:/tmp"
                 // println "deploying to tomcat mid"
