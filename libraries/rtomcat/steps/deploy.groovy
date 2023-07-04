d0860fc12e1ebe5e99685ea34cf40c99e0263bef
Vikas Salaria <vsalaria@dminc.com>
2023-07-04 11:55:16 +0530

    commit changes related to svn76

--- libraries/rtomcat/steps/deploy.groovy
+++ libraries/rtomcat/steps/deploy.groovy
@@ -11,8 +11,10 @@ void call()
             sshagent(['tomcat_deployment_cred']) {
                 def currentDir = sh(returnStdout: true,script: 'pwd').trim()
                 sh "ls  ${currentDir}"
-                sh "scp -o StrictHostKeyChecking=no **/*.war ${tomcat_url}:/opt/bitnami/tomcat/webapps"
+                sh "scp -o StrictHostKeyChecking=no **/*.war ${tomcat_url}:/tmp"
+                println "deploying to tomcat mid"
                 sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/bitnami/tomcat/bin/shutdown.sh"
+                println "deploying to tomcat mid1"
                 sh "ssh -o StrictHostKeyChecking=no ${tomcat_url} /opt/bitnami/tomcat/bin/startup.sh"
                                  
                     }
